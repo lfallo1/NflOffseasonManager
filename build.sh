027e39d50de2a53ecb62b92349e1c1a737416044
Lance Fallon <lancefallon@Lances-MBP.hsd1.md.comcast.net>
2017-04-30 21:53:36 -0400

    update build script. update message for notes area when not logged in

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo checking arguments
+ARG1="dummy"
+if [[ $# -eq 0 ]]; then
+  echo no argument supplied. will not restart tomcat
+else
+  ARG1=$1
+fi
+
 mvn install
 
 echo removing existing war file from machine
@@ -8,14 +16,15 @@ rm /Users/lancefallon/.m2/repository/com/lancefallon/usermgmt/nfl-draft-notes-ap
 echo renaming new build
 mv /Users/lancefallon/.m2/repository/com/lancefallon/usermgmt/nfl-draft-notes-app/0.0.1-SNAPSHOT/nfl-draft-notes-app-0.0.1-SNAPSHOT.war /Users/lancefallon/.m2/repository/com/lancefallon/usermgmt/nfl-draft-notes-app/0.0.1-SNAPSHOT/nfldraft.war
 
-echo stopping remote tomcat
-ssh -p 1033 -i /Users/lancefallon/.ssh/id_rsa.1488030491 nflcapsi@173.243.120.226 /home/nflcapsi/appservers/apache-tomcat-7.0.68/bin/shutdown.sh
+if [[ $ARG1 = "stopserver" ]]; then
+  echo stopping remote tomcat
+  ssh -p 1033 -i /Users/lancefallon/.ssh/id_rsa.1488030491 nflcapsi@173.243.120.226 /home/nflcapsi/appservers/apache-tomcat-7.0.68/bin/shutdown.sh
+fi
 
 echo deleting remote nfldraft application
 ssh -p 1033 -i /Users/lancefallon/.ssh/id_rsa.1488030491 nflcapsi@173.243.120.226 'rm -rf /home/nflcapsi/appservers/apache-tomcat-7.0.68/webapps/nfldraft*'
 
-echo copying new build to remote
-scp -i /Users/lancefallon/.ssh/id_rsa.1488030491 -P 1033 /Users/lancefallon/.m2/repository/com/lancefallon/usermgmt/nfl-draft-notes-app/0.0.1-SNAPSHOT/nfldraft.war nflcapsi@173.243.120.226:/home/nflcapsi/appservers/apache-tomcat-7.0.68/webapps
-
-echo starting remote tomcat
-ssh -p 1033 -i /Users/lancefallon/.ssh/id_rsa.1488030491 nflcapsi@173.243.120.226 /home/nflcapsi/appservers/apache-tomcat-7.0.68/bin/startup.sh
+if [[ $ARG1 = "stopserver" ]]; then
+  echo copying new build to remote
+  scp -i /Users/lancefallon/.ssh/id_rsa.1488030491 -P 1033 /Users/lancefallon/.m2/repository/com/lancefallon/usermgmt/nfl-draft-notes-app/0.0.1-SNAPSHOT/nfldraft.war nflcapsi@173.243.120.226:/home/nflcapsi/appservers/apache-tomcat-7.0.68/webapps
+fi
